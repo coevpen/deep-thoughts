91214f2036db18eeece93d982f55122be492676a
coevpen <koinuevins@gmail.com>
2022-03-22 16:59:46 -0500

    add GraphQL functions

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,29 @@
+const { User, Thought } = require('../models');
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
+      // get all users
+      users: async () => {
+        return User.find()
+          .select('-__v -password')
+          .populate('friends')
+          .populate('thoughts');
+      },
+      // get a user by username
+      user: async (parent, { username }) => {
+        return User.findOne({ username })
+          .select('-__v -password')
+          .populate('friends')
+          .populate('thoughts');
+      },
+      thoughts: async (parent, { username }) => {
+        //if username exists, use it as a parameter
+        const params = username ? { username } : {};
+        return Thought.find(params).sort({ createdAt: -1 });
+      },
+      // get a single thought
+      thought: async (parent, { _id }) => {
+        return Thought.findOne({ _id });
       }
     }
 };
